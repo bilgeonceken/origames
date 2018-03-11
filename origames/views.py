b2c237a7c87930d25ad82cb943488b0e23e83e7a
Bilge Önçeken <oncekenbilge@gmail.com>
2018-03-11 12:16:06 +0300

    logged out "/" now redirects to results page

--- origames/views.py
+++ origames/views.py
@@ -42,6 +42,7 @@ def home(request):
                     return redirect("home")
             return redirect("home")
     else:
+        return redirect("league/results")
         if request.user.is_authenticated:
             race = models.Race.objects.first()
             try:
