4d2e43e34b4f4f74c6f68a4bd5b0058cc5a3f8d8
Bilge Önçeken <oncekenbilge@gmail.com>
2018-03-14 20:12:54 +0300

    now you can sort tables with a click to a thead

--- static/league/js/script.js
+++ static/league/js/script.js
@@ -3,3 +3,10 @@ $(function(){
     $(this).toggleClass("open").next(".fold").toggleClass("open");
   });
 });
+
+$(document).ready(function()
+    {
+        $("#myTable1").tablesorter();
+        $("#myTable2").tablesorter();
+    }
+);
