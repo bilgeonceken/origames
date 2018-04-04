04dbcab2274989e2cb8c922df0ce6784bf8e452f
Bilge Önçeken <oncekenbilge@gmail.com>
2018-04-04 13:50:08 +0300

    e-mail is now required on sıgnup

--- origames/forms.py
+++ origames/forms.py
@@ -8,4 +8,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name','email' ,'password1', 'password2', )
